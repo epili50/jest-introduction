fab0a53df27dd70196328a2d755c89e3da92da68
epili50 <epili50@gmail.com>
2024-10-08 22:09:33 +0200

    funciona iteracion 3 (faltan tests)

--- pokerCards.js
+++ pokerCards.js
@@ -12,3 +12,5 @@ function selectCard(deck, card) {
     return undefined; // La carta no estaba en la baraja
 }
 
+
+module.exports = { selectCard };
--- pokerCards.test.js
+++ pokerCards.test.js
@@ -1,1 +1,13 @@
 // TODO: Importar selectCard !!
+const { selectCard } = require ('./pokerCards')
+
+it("Devuelve '3♠' y lo elimina del array de cartas", () =>{
+    const cards = ['A♠', '2♠', '3♠', '4♠', '5♠'];
+    const eraseCard = '3♠'
+    const selectedCard = selectCard(cards, eraseCard);
+    
+    expect(selectedCard).toBe(eraseCard);
+    expect(cards).not.toContain(eraseCard); 
+    expect(cards).toHaveLength(4)
+
+})
