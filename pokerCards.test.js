068e1fca0beed52d84fe8d5fd53ee9826fc7143e
epili50 <epili50@gmail.com>
2024-10-09 19:31:55 +0200

    listo iteracion 3

--- pokerCards.test.js
+++ pokerCards.test.js
@@ -10,4 +10,39 @@ it("Devuelve '3♠' y lo elimina del array de cartas", () =>{
     expect(cards).not.toContain(eraseCard); 
     expect(cards).toHaveLength(4)
 
-})
+});
+
+it("Devuelve 'A♥' y lo elimina del array de cartas", () =>{
+    const cards = ['A♥', 'K♦', '10♣', 'J♠'];
+    const eraseCard = 'A♥'
+    const selectedCard = selectCard(cards, eraseCard);
+    
+    expect(selectedCard).toBe(eraseCard);
+    expect(cards).not.toContain(eraseCard); 
+    expect(cards).toHaveLength(3)
+
+});
+
+
+it("Devuelve undefined y lo elimina del array de cartas", () =>{
+    const cards = ['7♠', '8♦', '9♣'];
+    const eraseCard = '10♣'
+    const selectedCard = selectCard(cards, eraseCard);
+    
+    expect(selectedCard).toBe(undefined);
+    expect(cards).not.toContain(eraseCard); 
+    expect(cards).toHaveLength(3)
+
+});
+
+it("Devuelve undefined y lo elimina del array de cartas", () =>{
+    const cards = ['7♠'];
+    const eraseCard = '7♠'
+    const selectedCard = selectCard(cards, eraseCard);
+    
+    expect(selectedCard).toBe(eraseCard);
+    expect(cards).not.toContain(eraseCard); 
+    expect(cards).toHaveLength(0)
+
+});
+
